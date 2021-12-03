98421a88fa0ab83716a16f38a15ee71d6f066826
alexlec2 <87832860+alexlec2@users.noreply.github.com>
2021-12-03 16:34:08 +0100

    Delete sonarQube01.java
    I delete the file to test the second java file

--- sonarQube01.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class sonarQube01 {
-    String addHeader(String stringToAdd, int x, int y){
-        return String.format("Added Header %s %d %d", stringToAdd, x, y);
-    }
-}
